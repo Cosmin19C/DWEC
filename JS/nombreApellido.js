c000556f1cdbad282504977a14a89958ebeacc9e
Cosmin <cosminostafe@gmail.com>
2024-10-22 19:26:44 +0200

    Creación de la rama

--- JS/nombreApellido.js
+++
@@ -1,8 +0,0 @@
-var nombre;
-var nombre = "Juan"; //Se declara y se asigna
-var nombre = "Juan", apellido = "García", apellido2 = "López";
-var nombreApellido = "Juan" + " " + "García";
-var nombreApellido = nombre + " " + apellido;
-var edad = nacimiento + 32;
-var numero = 23;
-var c = a + b;
--- JS/suma.js
+++
@@ -1,3 +0,0 @@
-window.alert(7+6);
-window.alert("Pues agarramela que me crece")
-window.alert("Pringao");
